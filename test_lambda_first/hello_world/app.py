18960052bf13686c8f217fc368909bbcc00864f2
ThaeThaeNaingGIC <thaethaenaing@gicjp.com>
2026-01-13 17:05:33 +0900

    update

--- test_lambda_first/hello_world/app.py
+++ test_lambda_first/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world & welcome to serverless computing! & test CI/CD pipeline",
+            "message": "hello world & welcome to serverless computing! & test CI/CD pipeline updated!!",
             # "location": ip.text.replace("\n", "")
         }),
     }
